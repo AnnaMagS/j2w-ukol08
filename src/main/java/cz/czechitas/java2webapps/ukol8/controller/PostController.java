2412826b7872dc40872003a4a6a4d4c86222629b
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-05-31 17:54:38 +0200

    first version

---
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
@@ -0,0 +1,21 @@
+package cz.czechitas.java2webapps.ukol8.controller;
+
+import cz.czechitas.java2webapps.ukol8.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class PostController {
+    private final PostService serivce;
+
+    @Autowired
+    public PostController(PostService serivce) {
+        this.serivce = serivce;
+    }
+
+    @GetMapping("/")
+    public Object seznam() {
+        return new ModelAndView("seznam")
+                .addObject("prispevky", this.serivce.list());
+    }
+}
---
+++ src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
@@ -0,0 +1,28 @@
+package cz.czechitas.java2webapps.ukol8.service;
+
+import cz.czechitas.java2webapps.ukol8.entity.Post;
+import cz.czechitas.java2webapps.ukol8.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> list(){
+        return postRepository.findAll();
+    }
+
+    public Post singlePost(String slug){
+        return postRepository.findBySlug(slug);
+    }
+}
