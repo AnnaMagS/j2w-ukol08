badcde1ced26d97cee6f79b9c51630cf34600e16
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-05-31 19:55:49 +0200

    Seznam funguje

--- src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/controller/PostController.java
@@ -2,9 +2,11 @@ package cz.czechitas.java2webapps.ukol8.controller;
 
 import cz.czechitas.java2webapps.ukol8.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class PostController {
     private final PostService serivce;
 
